317010b030798f5a99c5ea0424eb063c1df2f9a6
felix <felix@open-platform.kr>
2014-01-13 17:46:24 +0900

    이미지 빌더 추가

---
+++ Rebuilder/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Rebuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buildToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.buildToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(776, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // buildToolStripMenuItem
+            // 
+            this.buildToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem});
+            this.buildToolStripMenuItem.Name = "buildToolStripMenuItem";
+            this.buildToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.buildToolStripMenuItem.Text = "Build";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(776, 238);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 262);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "다중자막 이미지 파일 리빌더";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buildToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Rebuilder/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SubtitleBackoffice;
+using SubtitleBackoffice.ImageUtil;
+using SubtitleBackoffice.Subtitle;
+using SubtitleBackoffice.Utils;
+using SubtitleBackoffice.WorkThread;
+
+
+namespace Rebuilder
+{
+    public partial class Form1 : Form
+    {
+        private int DoneCounter=0;
+        private int CoreCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddText(string message)
+        {
+            MethodInvoker action = delegate
+            {
+                richTextBox1.Text += message + Environment.NewLine;
+                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                richTextBox1.ScrollToCaret();
+            };
+
+            richTextBox1.BeginInvoke(action);
+        }
+        public void ResultCallback(string str, string id)
+        {
+            DoneCounter++;
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            const string listQuery = @"select subtitle, ContentID,EpisodeNumber,CountryCode,Name,ID from subtitle";
+            const string countQuery = @"select count(*) from subtitle";
+
+            int count = 0;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Storage.DB_CONNECTION))
+                {
+                    con.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(countQuery, con))
+                    {
+                        SqlDataReader rdr = cmd.ExecuteReader();
+
+                        while (rdr.Read())
+                        {
+                            count = rdr.GetInt32(0);
+                        }
+                        con.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.WriteLine(ex.ToString());
+            }
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Storage.DB_CONNECTION))
+                {
+                    con.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(listQuery, con))
+                    {
+                        SqlDataReader rdr = cmd.ExecuteReader();
+
+                        int i = 0;
+                        while (rdr.Read())
+                        {
+                            DoneCounter = 0;
+
+                            for (int j = 0; j < CoreCount; j++)
+                            {
+                                Worker myWorker = new Worker(rdr.GetString(0), rdr.GetString(1), rdr.GetString(2), rdr.GetString(3), rdr.GetString(4), rdr.GetInt32(5).ToString(CultureInfo.InvariantCulture), new Worker.WorkerCallBack(ResultCallback), "BATCH");
+                                Thread myThread = new Thread(new ThreadStart(myWorker.Tasks));
+                                myThread.Start();
+
+                                string message = String.Format("{5}/{6} - {0},{1},{2},{3},{4}",
+                                    rdr.GetString(1), rdr.GetString(2), rdr.GetString(3), rdr.GetString(4), rdr.GetInt32(5).ToString(CultureInfo.InvariantCulture),
+                                    i, count);
+                                Log.WriteLine2(Storage.BASE_DIRECTORY + @"\dblog.txt", message);
+                                richTextBox1.Text += message + Environment.NewLine;
+                                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                                richTextBox1.ScrollToCaret();
+                                i++;
+                                rdr.Read();
+                            }
+                            while (DoneCounter < CoreCount) ;
+                            Log.WriteLine2(Storage.BASE_DIRECTORY + @"\dblog.txt", "SINGLE BATCH DONE");
+
+                        }
+
+                        con.Close();
+
+                        Log.WriteLine2(Storage.BASE_DIRECTORY + @"\dblog.txt", "GENERATION DONE");
+                        richTextBox1.Text += "GENERATION DONE" + Environment.NewLine;
+                        richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                        richTextBox1.ScrollToCaret();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.WriteLine(ex.ToString());
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CoreCount = Environment.ProcessorCount*2;
+            this.AddText(String.Format("Core count is {0}", CoreCount));
+
+        }
+
+    }
+}
